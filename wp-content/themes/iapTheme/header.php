e3afaf1aa10ee5a788fac62684fa99b29fc62d57
zsmarius <zsigmond.marius@gmail.com>
2019-05-08 18:33:10 +0300

    first commit

--- wp-content/themes/iapTheme/header.php
+++ wp-content/themes/iapTheme/header.php
@@ -2,6 +2,7 @@
 <html>
 
 <head>
+<link href="https://fonts.googleapis.com/css?family=PT+Serif:400,400i" rel="stylesheet">
     <?php wp_head(); ?>
 </head>
 
--- wp-content/themes/iapTheme/index.php
+++ wp-content/themes/iapTheme/index.php
@@ -1,3 +1,19 @@
-<?php get_header(); ?>
+<?php get_header();
+if (have_posts()) : 
+        // Start the loop.
+        while (have_posts()) : the_post(); ?>
+            <h1><?php the_title();?></h1>
+            <?php the_content();?>
+            <?php the_date();?>
+            <?php the_category();?>
+            <?php echo '<hr>';?>
+<?php
+        // End the loop.
+        endwhile;
 
+    // If no content, include the "No posts found" template.
+    else :
+
+    endif;
+    ?>
 <?php get_footer(); ?>
